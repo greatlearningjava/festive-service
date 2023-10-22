a5f0785e52070c91746e77e20bdcdd67ac867d0f
Manoj Kumar <manoj.er.kumar@gmail.com>
2023-10-22 05:37:12 +0530

    Tree creation by entering the array elements

--- src/main/java/com/greatlearning/algo/tree/TreeMain.java
+++ src/main/java/com/greatlearning/algo/tree/TreeMain.java
@@ -5,25 +5,76 @@ import java.util.Scanner;
 public class TreeMain {
 
     public static void main(String[] args) {
-//        Node node = new Node(10);
-//        Node left = new Node(5);
-//        Node right = new Node(20);
-//
-//        node.left = left;
-//        node.right = right;
 
-        Node root = usingUserChoice();
-        preOrder(root);
+//        Node root = usingUserChoice();
+//        System.out.println("Traverse using the preorder");
+//        preOrder_Traversal(root);
+//        System.out.println("Traverse using the inorder");
+//        inOrder_Traversal(root);
+        int[] elements = {10, 5, 10, 2, 12, 30};
+        createBinarySearhTree(elements);
+        System.out.println("Traverse using the inorder");
+        inOrder_Traversal(root);
+        System.out.println("Traverse using the preorder");
+        preOrder_Traversal(root);
+
     }
 
-    private static void preOrder(Node root) {
-        if(root == null){
+    private static void inOrder_Traversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder_Traversal(root.left);
+        System.out.print(root.value + " ");
+        inOrder_Traversal(root.right);
+
+    }
+
+    private static void preOrder_Traversal(Node root) {
+        if (root == null) {
             return;
         }
         System.out.print(root.value + " ");
-        preOrder(root.left);
-        preOrder(root.right);
+        preOrder_Traversal(root.left);
+        preOrder_Traversal(root.right);
 
+    }
+
+    public static Node createBinarySearhTree(int[] elements) {
+        Node root = null;
+        Scanner choice = new Scanner(System.in);
+        //char ch = 'y';
+        System.out.println("Enter the choice(y/n): ");
+
+        int index = 0;
+
+        while (index < elements.length) {
+            System.out.println("Enter the Node Value: " + elements[index]);
+            Node temp = new Node(elements[index]);
+            if (root == null) {
+                root = temp;
+            } else {
+                Node prev = null;
+                Node head = root;
+
+                while (head != null) {
+                    prev = head;
+                    if (temp.value < head.value) {
+                        head = head.left;
+                    } else if (temp.value > head.value) {
+                        head = head.right;
+                    }
+                }  // while loop closed
+
+                if (temp.value < prev.value) {
+                    prev.left = temp;
+                } else if (temp.value > prev.value) {
+                    prev.right = temp;
+                }
+            }
+            index++;  // increment the index
+        } // First while loop closed
+        return root;
     }
 
     static Node usingUserChoice() {
@@ -63,7 +114,7 @@ public class TreeMain {
             System.out.println("Enter the choice(y/n): ");
             ch = choice.next().charAt(0);
         } // First while loop closed
-    return root;
+        return root;
     }
 
 }
