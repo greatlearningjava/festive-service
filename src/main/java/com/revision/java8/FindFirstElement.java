1afe8e862a2bc6d748ee9466d3c4912172dc223a
Manoj Kumar <manoj.er.kumar@gmail.com>
2024-02-18 23:03:33 +0530

    Given the list of integers, find the first element of the list using Stream functions

---
+++ src/main/java/com/revision/java8/FindFirstElement.java
@@ -0,0 +1,13 @@
+package com.revision.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Given the list of integers, find the first element of the list using Stream functions?
+public class FindFirstElement {
+    public static void main(String[] args) {
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+        myList.stream().findFirst().ifPresent(System.out::println);
+
+    }
+}
