f4546170a0adaf47bbfed32c0ae02e1911a78e58
Manoj Kumar <manoj.er.kumar@gmail.com>
2024-02-18 23:01:28 +0530

    Remove the duplicate from list

--- src/main/java/com/revision/java8/DuplicateElements.java
+++ src/main/java/com/revision/java8/DuplicateElements.java
@@ -1,7 +1,13 @@
 package com.revision.java8;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class DuplicateElements {
     public static void main(String[] args) {
-        
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+        HashSet set = new HashSet();
+        myList.stream().filter(element -> !set.add(element)).forEach(System.out::println);
     }
 }
